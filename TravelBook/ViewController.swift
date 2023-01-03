013d43f647c24c8015b800d4fb61d9fb8718efcb
zynpbyrkdmrl <zeynepbayrak863@gmail.com>
2023-01-03 15:58:12 +0300

    pinleme dahil

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation // kullanıcının konumunu almak için kulanacagımız sınıf
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate { //view controllera bu sınıflardan bazı fonksiyonlara erişim yetkisi vermemiz için bu sınıfları belirtmemiz gerekiyor.
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        mapView.delegate = self // mapview ın delegasyonunu viewcontroller olarak değiştirmemiz gerekiyor.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // birebir nokta atışı konum belirlemek istersem.
+        locationManager.requestWhenInUseAuthorization() // en mantıklısı bu uygulamayı kullanırken izin istemek
+        locationManager.startUpdatingLocation()
+        
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(gestureRecognizer) // haritamıza ekledik
     }
-
-
+    
+    @objc func chooseLocation (gestureRecognizer: UILongPressGestureRecognizer){ //fonksyionun içine değişken yazdık farklı olarak. bu fonksiyonun içinde gestureRecognizera ulaşmak istiyorum.
+        if gestureRecognizer.state == .began { //gestureRecognizer başladıysa eğer
+            let touchedPoint = gestureRecognizer.location(in: self.mapView) // dokunulan nokta
+            let touchedCoordinates = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)//dokunulan noktayı koordinata çeviriyoruz.
+            
+            let annotation = MKPointAnnotation() // annotation = pin oluşturuyoruz.
+            annotation.coordinate = touchedCoordinates
+            annotation.title = "New Annotation"
+            annotation.subtitle = "Travel Book"
+            self.mapView.addAnnotation(annotation)//haritamıza ekledik
+        }
+        
+    }
+    
+    //güncellenen lokasyonları bana dizi içerisinde veren fonksiyon
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = CLLocationCoordinate2D (latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude) //latitude:enlem, longitude:boylam
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05) // zoom seviyesine span denilir. ne kadar küçültürsek o kadar zoomlar
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
+    
+    
+    
 }
 
